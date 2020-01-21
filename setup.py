4aaf14f96aa84e526b6a5e893f025abf03064808
Mathieu Lecarme <mlecarme@bearstech.com>
2020-01-21 18:08:27 +0100

    Black.

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aiohttp-oauth2-gitlab',
-    version='0.1',
-    description='',
-    url='https://github.com/factorysh/aiohttp-oauth2-gitlab',
-    install_requires=[
-        'aioauth-client',
-        'aiohttp_session[secure]',
-    ],
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'example']),
-    extras_require={
-        'test': ['pytest', 'pytest-cov', 'pytest-mock', 'pytest-asyncio'],
-    },
+    name="aiohttp-oauth2-gitlab",
+    version="0.1",
+    description="",
+    url="https://github.com/factorysh/aiohttp-oauth2-gitlab",
+    install_requires=["aioauth-client", "aiohttp_session[secure]",],
+    packages=find_packages(exclude=["contrib", "docs", "tests", "example"]),
+    extras_require={"test": ["pytest", "pytest-cov", "pytest-mock", "pytest-asyncio"],},
     license="3 terms BSD",
     classifiers=[
-      'Operating System :: MacOS',
-      'Operating System :: POSIX',
-      'License :: OSI Approved :: BSD License',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-    ]
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
